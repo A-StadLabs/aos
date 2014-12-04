839c67368ac9f6a8954927db4e9d427d56242a0e
kingflurkel <mikethuy@gmail.com>
2014-12-04 19:45:55 +0100

    notificaties en user login

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var request = require('request');
+var toughcookie = require('tough-cookie');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -10,7 +11,6 @@ app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
 
-
 app.get('/searching', function(req, res){
 	var val = req.query.search;
  	//console.log(val);
@@ -18,13 +18,14 @@ app.get('/searching', function(req, res){
  	request.post({
   		headers: {'Content-Type' : 'application/json'},
   		method: 'post',
+  		jar: true,
   		url:     'https://www.antwerpen.be/srv/content/d/search?save=true',
   		json: {"type":[{"key":9,"value":"Infofiche","type":"type"}],"titel":[{"key":"titel","value":val,"type":"titel"}]}
 	}, function(error, response, body){
-  		console.log('Body die terug komt? ',body);
+  		console.log('Body die terug komt? ',body, ' response: ', response);
+  		res.send(body);
 	});
 });
-
 
 app.get('/login', function(req, res){
 	var user = req.query.username;
@@ -33,10 +34,23 @@ app.get('/login', function(req, res){
  	request.post({
   		headers: {'Content-Type' : 'application/json'},
   		method: 'post',
+  		jar: true,
   		url:     'https://www.antwerpen.be/srv/user/d/auth',
   		json: {"username": user, "password": pass}
 	}, function(error, response, body){
   		console.log('Body die terug komt? ',body, '////// Response: ', response);
+  		console.log('cookie? ',response.headers['set-cookie']);
   		res.send(body);
 	});
 });
+
+
+app.get('/notifications', function(req, res){
+ 	//console.log(val);
+ 	request.get({url: 'https://www.antwerpen.be/srv/notification/d/unread', jar: true}, function(error, response, body){
+  		console.log('Body die terug komt? ',body, '////// Response: ', response);
+  		res.send(body);
+	});
+});
+
+
