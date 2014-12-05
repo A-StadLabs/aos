568646a267ed56696dcd1889760ef9da354c2439
kingflurkel <mikethuy@gmail.com>
2014-12-05 15:01:56 +0100

    sessions?

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ var express = require('express');
 var app = express();
 var request = require('request');
 var toughcookie = require('tough-cookie');
+var session = require('express-session');
 
 app.set('port', (process.env.PORT || 5000));
 
 // serveer de polymeer
 app.use(express.static(__dirname + '/public'));
+
+app.use(session({
+  secret: 'benissangekkie',
+  resave: true,
+  saveUninitialized: true
+}));
 
 // gewuun ne status update
 app.listen(app.get('port'), function() {
@@ -24,7 +31,6 @@ app.get('/searching', function(req, res){
   		url:     'https://www.antwerpen.be/srv/content/d/search?save=true',
   		json: {"type":[{"key":9,"value":"Infofiche","type":"type"}],"titel":[{"key":"titel","value":val,"type":"titel"}]}
 	}, function(error, response, body){
-  		console.log('Body die terug komt? ',body, ' response: ', response);
   		res.send(body);
 	});
 });
@@ -41,17 +47,25 @@ app.get('/login', function(req, res){
   		url:     'https://www.antwerpen.be/srv/user/d/auth',
   		json: {"username": user, "password": pass}
 	}, function(error, response, body){
-  		console.log('Body die terug komt? ',body, '////// Response: ', response);
-  		console.log('cookie? ',response.headers['set-cookie']);
+		req.session.userobject = body;
   		res.send(body);
 	});
+});
+
+app.get('/user', function(req, res){
+	if(req.session.userobject){
+		res.send(req.session.userobject);
+	} else {
+		res.send({ 'status': 'E', 'msg': 'User not logged in.'});
+	}
+
 });
 
 // Notificaties 
 app.get('/notifications', function(req, res){
  	//console.log(val);
+ 	req.session.userobject;
  	request.get({url: 'https://www.antwerpen.be/srv/notification/d/unread', jar: true}, function(error, response, body){
-  		console.log('Body die terug komt? ',body, '////// Response: ', response);
   		res.send(body);
 	});
 });
@@ -61,7 +75,6 @@ app.get('/infofichedetail', function(req, res){
  	//console.log(val);
  	var val = req.query.infokey;
  	request.get({url: 'https://www.antwerpen.be/srv/content/d/detail/id/'+val, jar: true}, function(error, response, body){
-  		console.log('Body die terug komt? ',body, '////// Response: ', response);
   		res.send(body);
 	});
 });
