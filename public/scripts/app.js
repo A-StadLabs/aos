38e74918bd65b0801cb40a236b43121d36fa5258
kingflurkel <mikethuy@gmail.com>
2014-12-18 23:37:49 +0100

    link posten

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,6 +8,12 @@
     console.log('aOS is ready to rock!');
   });
 
+  document.addEventListener('to-Top', function() {
+    // Perform some behaviour
+    console.log('Its lonely at the top.');
+    document.querySelector('#mainContainer').scroller.scrollTop = 0;
+  });
+
 // wrap document so it plays nice with other libraries
 // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
 })(wrap(document));
