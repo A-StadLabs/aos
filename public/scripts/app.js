76c8a4877915e09096f0dd5d3b2611ff2a074fc1
kingflurkel <mikethuy@gmail.com>
2015-01-05 15:16:48 +0100

    Revert 6bd1cdc..34cd91a
    This rolls back to commit 6bd1cdc2919d650da578b41708ba8d2a5b556d2f.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,4 @@
+
 (function(document) {
   'use strict';
 
@@ -22,6 +23,14 @@
     document.getElementById('toast').show();
   });
 
+  document.addEventListener('show-sharing', function(e) {
+    // Perform some behaviour
+    console.log('Share: ', e.detail.url);
+    document.getElementById('share').link = e.detail.url;
+    document.getElementById('share').tekst = e.detail.tekst;
+    document.getElementById('overlay').toggle();
+  });
+
 
 
   
@@ -31,3 +40,5 @@
 // wrap document so it plays nice with other libraries
 // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
 })(wrap(document));
+
+
